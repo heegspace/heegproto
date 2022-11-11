6e58104682042fe2ec587caa9778fa1a4e66ad83
sunning <sunning@126.com>
2022-11-11 11:09:44 +0800

    fix: 修复

--- datanode/datanode.pb.go
+++ datanode/datanode.pb.go
@@ -7,7 +7,7 @@
 package datanode
 
 import (
-	common "github.com/heegspace/heegprotocommon"
+	common "github.com/heegspace/heegproto/common"
 	rescode "github.com/heegspace/heegproto/rescode"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
